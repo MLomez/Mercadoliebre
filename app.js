17b142bb7c5ef39cfe2dd45a19d6661200c70c33
MLomez <mlomez2017@gmail.com>
2022-09-07 19:08:21 -0300

    Modificaciones al html y css

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const path = require ("path");
 
 let app= express ();
 
-app.listen(3000)
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req,res) =>{
+    res.sendFile((__dirname + '/views/home.html'));  
+});
+
+app.listen(3000, function () {
+    console.log("Servidor Corriendo");
+})
+ 
